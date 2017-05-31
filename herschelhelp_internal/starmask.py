c40aaa992b2d27dfe6eede1f66692eb1e0e72678
Raphael Shirley <Raphael.Shirley@sussex.ac.uk>
2017-05-31 17:58:17 +0100

    reg to moc seems to be working - issues with moc order

--- herschelhelp_internal/starmask.py
+++ herschelhelp_internal/starmask.py
@@ -1,19 +1,29 @@
 import logging
-from collections import Counter
+#from collections import Counter
 
-import matplotlib as mpl
+#import matplotlib as mpl
 import numpy as np
-import seaborn as sns
+#import seaborn as sns
 from astropy import units as u
-from astropy.coordinates import SkyCoord
-from astropy.table import Column, hstack, vstack
-from matplotlib import pyplot as plt
+#from astropy.coordinates import SkyCoord
+from astropy.table import Table
+#from astropy.io import fits
+from astropy.wcs import WCS
+
+import pyregion
+#import pyregion._region_filter as filter
+#import pyregion._region_filter as region_filter
+
+
+from pymoc import MOC
+import healpy as hp
+from scipy.constants import pi
 
 
 LOGGER = logging.getLogger(__name__)
 
 
-def create_starmask(gaia, radius = 10 * u.arcsec):
+def create_starmask(gaia, target, radius = 10 * u.arcsec):
     """Create a ds9 region file of circles around every GAIA object
 
     This function loops through every object in GAIA in a given field and writes
@@ -25,6 +35,8 @@ def create_starmask(gaia, radius = 10 * u.arcsec):
     ----------
     gaia: string
         Location of the fits table of GAIA objects in the target field.
+    target: string
+        Location of output region file.
     radius: astropy quantity (distance)
         Radius for considering sources as duplicates.
 
@@ -34,18 +46,122 @@ def create_starmask(gaia, radius = 10 * u.arcsec):
         Location of produced starmask .reg file.
 
     """
-    print('test')
-    #stars = Table.open(gaia)
-    starmask = 'final location'
-    return starmask
+    
+    
+    
+    stars = Table.read(gaia)['field', 'ra','dec']
+
+    print('There are ' + str(len(stars)) + ' GAIA stars in ' + stars[0]['field'])
+    #for star in stars:
+
+    #write the ds9regions to file
+    f = open(target, 'w+')
+    
+    for star in stars:
+        f.writelines('circle ' 
+                     + str(star['ra']) + ' ' 
+                     + str(star['dec']) + ' '
+                     + str(radius/u.arcsec) + '"\n')
     
 
-def ds9tomoc(ds9file, order = 13):
-    """Convert a ds9 region file into a moc
+    f.close()
+    print( 'Starmask (basic 10 arcsec circles) written to ' + target)
+    #return 'starmask written to data/starmask.reg'
+    
+
+def reg2moc(region_file, fieldmoc, order = 12):
+    """Convert a ds9 region file into MOC/fits format
     
     This function takes a ds9 region file and converts it to a MOC of given order. 
     
     The default order is 13.
+    
+    Parameters
+    ----------
+    region_file: string
+        Location of ds9 region file.
+    fieldmoc: string
+        Location of the MOC describing the unmasked field 
+    order: int
+        Order of MOC to use
+
+    Returns
+    -------
+    string
+        Location of produced starmask MOC .fits file.
     """
+    
+    #The code requires an image WCS
+    w = WCS("""
+WCSAXES =                    2 / Number of coordinate axes
+CRPIX1  =                240.5 / Pixel coordinate of reference point
+CRPIX2  =                120.5 / Pixel coordinate of reference point
+CDELT1  =               -0.675 / [deg] Coordinate increment at reference point
+CDELT2  =                0.675 / [deg] Coordinate increment at reference point
+CUNIT1  = 'deg'                / Units of coordinate increment and value
+CUNIT2  = 'deg'                / Units of coordinate increment and value
+CTYPE1  = 'GLON-AIT'           / galactic longitude, Hammer-Aitoff projection
+CTYPE2  = 'GLAT-AIT'           / galactic latitude, Hammer-Aitoff projection
+CRVAL1  =                  0.0 / [deg] Coordinate value at reference point
+CRVAL2  =                  0.0 / [deg] Coordinate value at reference point
+LONPOLE =                  0.0 / [deg] Native longitude of celestial pole
+LATPOLE = 90.0 / [deg] Native latitude of celestial pole
+""")
+    
+    # The ds9 -> MOC converter
+    
+    # Use the HELP-coverage moc of the field you want to use
+    # available at hedam.lam.fr/HELP/coverages.html
+    # example is with the XMM-LSS MOC
+    #moc_file = 'XMM-LSS_MOC.fits'
+    moc = MOC(filename = fieldmoc)
+    
+    # The order of detail for the final MOC which  incorporates the region file
+    
+    NSIDE = hp.order2nside(order)
+    
+    # calculate the ra and dec of every healpix pixel of order = ORDER
+    hp_idx = list(moc.flattened(order))
+    theta, phi = hp.pix2ang(NSIDE, hp_idx, nest = True)
+    ra,dec = np.degrees(phi), -np.degrees(theta-pi/2)
+    x,y = w.wcs_world2pix(ra,dec,0)
+    
+    # Opens region file, with wcs, needs wcs to convert to pixel coordinates to make the inside function work.
+    # If the ds9 region file is in pixel coordinates than the relevent wcs should be used, if not than the suplied
+    # wcs will apply the same conversion to the map and the region file.
+    
+    r = pyregion.open(region_file).as_imagecoord(w)
+    # from r a certain region can be selected if not all regions are relevant, for instance r = r[0] or r = [3:9]
+    
+    r2 = r.get_filter(w)
+    
+    # bad_region_mask = TRUE if the region file contains the regions which should be removed from the MOC
+    # bad_region_mask = FALSE if the region file contains the regions which should be kept.
+    bad_region_mask = True
+    if bad_region_mask == True:
+        in_range = np.ones(np.size(x), dtype=bool)
+        mask = r2.inside(x,y)
+        in_range[mask] = False
+        mask = in_range
+    else:
+        mask = r2.inside(x,y)
+    
+    # if there are several different ds9 region files:
+    # (in shell) >>> ls *reg > region_files.txt
+    # in python:
+    # name = np.loadtxt('region_files.txt',usecols = [0],dtype = str)
+    # in_range = np.ones(np.size(x), dtype=bool)
+    # for j in range(0,np.size(name)):
+    #    r = pyregion.open(name[j]).as_imagecoord(w)
+    #    r2 = r.get_filter(w2)
+    #    in_mask = r2.inside(x,y)
+    #    in_range[in_mask] = False
+    # mask = in_range
+    
+    # creates new MOC, with the ds9 region taken out OR only keeping the ds9 region
+    hp_idx = np.array(hp_idx)
+    MOC(order=order,cells = hp_idx[mask]).write("NEW_MOC_O"+str(order)+".fits")
 
-
+if __name__ == '__main__':
+    #create_starmask('data/GAIA_CDFS-SWIRE.fits', 'data/starmask_CDFS-SWIRE.reg')
+    reg2moc('data/starmask_CDFS-SWIRE.reg','data/CDFS-SWIRE_MOC.fits', order=13)
